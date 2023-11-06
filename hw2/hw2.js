459755f2b0c29a68000d2f2971ac027c9b77ad9c
Hnatenkov <hnatenko.vadim3@gmail.com>
2023-11-06 23:03:01 +0200

    1

---
+++ hw2/hw2.js
@@ -0,0 +1,27 @@
+//1. Чому будуть дорівнювати значення змінніх a, b та result після виконання наступних операцій?.
+// let a = 1 + 3 - 5 * 2;
+// let b;
+// b = 2 ** 3 / 5;
+// const c = 7;
+// let result = a + b * c;
+
+console.log('=======1=======');
+let a1;
+a1 = 1 + 3 - 5 * 2;
+
+console.log('a=', a1);
+let b1;
+b1 = 2 ** 3 / 5;
+console.log('b=', b1);
+const c1 = 7;
+
+let result;
+result= a1 + b1 * c1;
+
+console.log('result=', result);
+
+// 2. Розв"яжіть за допомогою мови програмування JS квадратне рівняння 4x^2 - 5x + 1 = 0
+console.log('=======2=======');
+
+
+
